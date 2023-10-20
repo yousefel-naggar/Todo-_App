a4c078c54521be6d74c272fe5d0bf5d6736b1ae1
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-20 17:55:23 +0300

    theme of app bar

---
+++ lib/shared/style/mytheme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme = ThemeData(
+      colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: Color(0xFF5D9CEC),
+          onPrimary: Color(0xFF383838),
+          secondary: Colors.white,
+          onSecondary: Color(0xFFCDCACA),
+          error: Colors.red,
+          onError: Colors.grey,
+          background: Colors.white,
+          onBackground: Color(0xFF61E757),
+          surface: Colors.white,
+          onSurface: Color(0xFFA9A9A9)),
+      scaffoldBackgroundColor: Color(0xFFDFECDB),
+      appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF5D9CEC),
+          titleTextStyle: GoogleFonts.poppins(
+            fontSize: 22.sp
+          )));
+}
