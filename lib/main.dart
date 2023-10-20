12a5ea160e831921e9c3a00958ae446a93d3e541
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-20 17:54:22 +0300

    wrapping material app with ScreenUtilInit to make the app responsive and adding theme file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo/layout/home_layout.dart';
+import 'package:todo/shared/style/mytheme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          routes: {
+            HomeLayout.routeName: (context) => HomeLayout(),
+          },
+          initialRoute: HomeLayout.routeName,
+          theme: MyTheme.lightTheme,
+        );
+      },
     );
   }
 }
-
-
