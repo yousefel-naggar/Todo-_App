862524284b2c27319d95e2dd107fc93552ba3f46
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-25 00:09:03 +0300

    make the task slidable to we can edit or delete task

--- lib/screens/tasks/task_item.dart
+++ lib/screens/tasks/task_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:todo/models/task_model.dart';
 
 class TaskItem extends StatelessWidget {
@@ -14,50 +15,69 @@ class TaskItem extends StatelessWidget {
       child: Card(
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(13.r)),
-        child: Padding(
-          padding: REdgeInsets.all(12.0),
-          child: Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(12.r)),
-                height: 80.h,
-                width: 5.w,
-              ),
-              SizedBox(
-                width: 12.w,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    taskModel.taskTittle,
-                    style: TextStyle(fontSize: 18.sp),
-                  ),
-                  SizedBox(
-                    height: 2.h,
-                  ),
-                  Text(taskModel.taskDetails,
-                      style: TextStyle(fontSize: 15.sp)),
-                ],
-              ),
-              Spacer(),
-              InkWell(
-                onTap: () {},
-                child: Container(
+        child: Slidable(
+          startActionPane: ActionPane(motion: BehindMotion(), children: [
+            SlidableAction(
+              onPressed: (context) {},
+              label: "Delete",
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(13.r),
+                  bottomLeft: Radius.circular(13.r)),
+            ),
+            SlidableAction(
+              onPressed: (context) {},
+              label: "Edit",
+              icon: Icons.edit,
+              backgroundColor:Theme.of(context).colorScheme.primary,
+            ),
+          ]),
+          child: Padding(
+            padding: REdgeInsets.all(12),
+            child: Row(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.r),
-                      color: Theme.of(context).colorScheme.primary),
-                  height: 30.h,
-                  width: 50.w,
-                  child: Icon(
-                    Icons.done,
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12.r)),
+                  height: 80.h,
+                  width: 5.w,
                 ),
-              )
-            ],
+                SizedBox(
+                  width: 12.w,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      taskModel.taskTittle,
+                      style: TextStyle(fontSize: 18.sp),
+                    ),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    Text(taskModel.taskDetails,
+                        style: TextStyle(fontSize: 15.sp)),
+                  ],
+                ),
+                Spacer(),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.r),
+                        color: Theme.of(context).colorScheme.primary),
+                    height: 30.h,
+                    width: 50.w,
+                    child: Icon(
+                      Icons.done,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
